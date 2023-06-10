63dedd2d7982d4855699fcdc8449d92fa2827d45
adisatya2 <89624834+adisatya2@users.noreply.github.com>
2023-06-11 03:37:37 +0800

    update versi 3.2.2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,64 @@
 @extends('layouts.auth')
 @section('title', 'Login')
 @section('loginbox')
-    <div class="login-box shadow p-0 mb-5 bg-white rounded">
-        <!-- /.login-logo -->
-        <div class="card card-outline card-success">
-            <div class="card-header text-center">
-                @if ($setting)
-                    @if ($setting->path_logo_login)
-                        <img src="{{ asset('storage/logo/' . $setting->path_logo_login) }}" height="100px" alt="">
-                    @endif
-                @else
-                    <img src="{{ asset('/') }}dist/img/logo hermina horozontal.png" height="100px" alt="">
-                @endif
-            </div>
-            <div class="card-body">
-                <p class="login-box-msg">Sign in to start your session</p>
+<div class="login-box shadow p-0 mb-5 bg-white rounded">
+    <!-- /.login-logo -->
+    <div class="card card-outline card-success">
+        <div class="card-header text-center">
+            @if ($setting)
+            @if ($setting->path_logo_login)
+            <img src="{{ asset('storage/logo/' . $setting->path_logo_login) }}" height="100px" alt="">
+            @endif
+            @else
+            <img src="{{ asset('/') }}dist/img/logo hermina horozontal.png" height="100px" alt="">
+            @endif
+        </div>
+        <div class="card-body">
+            <p class="login-box-msg">Sign in to start your session</p>
 
-                <form action="{{ route('login') }}" method="post">
-                    @csrf
-                    <div class="input-group">
-                        <input type="text" class="form-control @error('username') is-invalid @enderror" name="username"
-                            id="username" value="{{ old('username') }}" placeholder="Username">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fa-fw fas fa-id-card"></span>
-                            </div>
+            <form action="{{ route('login') }}" method="post">
+                @csrf
+                <div class="input-group">
+                    <input type="text" class="form-control @error('username') is-invalid @enderror" name="username"
+                        id="username" value="{{ old('username') }}" placeholder="Username">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fa-fw fas fa-id-card"></span>
                         </div>
                     </div>
-                    @error('username')
-                        <small class="error text-danger">{{ $message }}</small>
-                    @enderror
-                    <div class="input-group mt-3">
-                        <input type="password" class="form-control @error('password') is-invalid @enderror" name="password"
-                            id="password" placeholder="Password">
-                        <div class="input-group-append">
-                            <div class="input-group-text" onclick="myFunction()">
-                                <span id="icon" class="fa-fw fas fa-eye"></span>
-                            </div>
+                </div>
+                @error('username')
+                <small class="error text-danger">{{ $message }}</small>
+                @enderror
+                <div class="input-group mt-3">
+                    <input type="password" class="form-control @error('password') is-invalid @enderror" name="password"
+                        id="password" placeholder="Password">
+                    <div class="input-group-append">
+                        <div class="input-group-text" onclick="myFunction()">
+                            <span id="icon" class="fa-fw fas fa-eye"></span>
                         </div>
                     </div>
-                    @error('password')
-                        <small class="error text-danger">{{ $message }}</small>
-                    @enderror
-                    <div class="row mt-3">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="remember">
-                                <label for="remember">
-                                    Remember Me
-                                </label>
-                            </div>
-                        </div>
-                        <div class="col-4">
-                            <button type="submit" class="btn btn-success btn-block">Sign In</button>
+                </div>
+                @error('password')
+                <small class="error text-danger">{{ $message }}</small>
+                @enderror
+                <div class="row mt-3">
+                    <div class="col-8">
+                        <div class="icheck-primary">
+                            <input type="checkbox" id="remember">
+                            <label for="remember">
+                                Remember Me
+                            </label>
                         </div>
                     </div>
-                </form>
-            </div>
-            <div class="card-footer">
-                <p class="m-0 text-center">
-                    <a href="{{ route('register') }}" class="text-muted">Register a new account</a>
-                </p>
-            </div>
+                    <div class="col-4">
+                        <button type="submit" class="btn btn-success btn-block">Sign In</button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
+</div>
 @endsection
 <script>
     function myFunction() {
