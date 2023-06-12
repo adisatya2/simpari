c7f87adcffb5d04801e81a2a97b454265d80a5d5
adisatya2 <89624834+adisatya2@users.noreply.github.com>
2023-06-12 10:51:31 +0800

    update versi 3.2.4

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,6 +3,6 @@
         href="https://adminlte.io">AdminLTE.io</a>.
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.2.2
+        <b>Version</b> 3.2.4
     </div>
 </footer>
